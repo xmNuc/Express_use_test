2b905bb6842b9f3ec5fd4f7fe86616936ad47fd1
xmNuc <xmnuc@o2.pl>
2021-11-17 00:29:47 +0100

    1.1 commit

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 const express = require('express');
 
 const app = express();
+
+const port = 3000;
+const host = 'localhost';
+
+app.get(
+  '/',
+  (req, res) => res.send('<h1>Express serv is online</h1>')
+  //   console.log('Serv is live')
+);
+
+app.listen(port, host, () =>
+  console.log(`Server has started on ${host} ${port}`)
+);

1171c22c7548fd45e347c203f3945ea7ea494d28
xmNuc <xmnuc@o2.pl>
2021-11-17 01:14:25 +0100

    2.4 commit

--- app.js
+++ app.js
@@ -16,6 +16,22 @@ app.get('/scripts/script.js', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'scripts', 'script.js'));
 });
 
+app.all('/methods', (req, res) => {
+  console.log(req.method);
+  console.log(req.ips);
+  console.log(req.ip);
+  console.log(req.path);
+  console.log(req.url);
+  console.log(req.originalUrl);
+  console.log(req.protocol);
+  console.log(req.secure);
+  console.log(req.hostname);
+
+  res.send(
+    `Methods ${req.method}, ${req.ips}, ${req.ip}, ${req.path}, ${req.url}, ${req.originalUrl}, ${req.protocol}, ${req.secure}, ${req.hostname}`
+  );
+});
+
 app.listen(port, host, () =>
   console.log(`Server has started on ${host} ${port}`)
 );
